91b61b1d71f95a4195790cd8e0711b940f18e0ed
juliana201291 <jjulna2@gmail.com>
2026-09-10 09:54:10 -0300

    docs: comentario na funcao calculat total

---
+++ app.js
@@ -0,0 +1,16 @@
+
+
+
+function calcularTotal (itens) {
+    let total = 0
+
+for (let i=0; i < itens.length; i++){
+    total += itens[i].preco
+}
+
+//aplica desconto de videlidade
+//antes de retornar o valor final
+
+
+return total
+}
